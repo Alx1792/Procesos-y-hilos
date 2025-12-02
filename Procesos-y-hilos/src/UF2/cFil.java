a2035f5d26736c596dccf62ad1c02dba5e27e4a9
Ferripro321 <ferrangimenezfiguerola@alumnes.ilerna.com>
2025-12-02 16:13:03 +0100

    ex 1.2

--- Procesos-y-hilos/src/UF2/cFil.java
+++ Procesos-y-hilos/src/UF2/cFil.java
@@ -20,7 +20,7 @@ public class cFil implements Runnable {
         System.out.println("Iniciant execució procés " + aNomFil);
         try {
             for (int vComptador = 0; vComptador < 10; vComptador ++) {
-                Thread.sleep(aTemporitzacio);
+                Thread.sleep(2000);
                 System.out.println("Despertant aturada " + vComptador + " procès " + aNomFil);
             }
         }
