afd628f768dbd6a1718d2c25e1fc7dc8b6725178
FGF2025 <ferrangimenezfiguerola@alumnes.ilerna.com>
2025-12-08 16:27:12 +0100

    Acabat 1.4
    Modificat formula de temorització per simplificar-ho i fegit ultims canvis.

--- Procesos-y-hilos/src/UF2/cFilPrincipal.java
+++ Procesos-y-hilos/src/UF2/cFilPrincipal.java
@@ -3,37 +3,41 @@ package UF2;
 import java.util.Scanner;
 
 public class cFilPrincipal {
+    public static void main (String [] pArguments) throws InterruptedException {
+        Scanner scan =new Scanner(System.in);
+        System.out.println("Quants fils vols crear");
+        boolean invalid = true;
+        int fils = scan.nextInt();
+        while (invalid) {
+            if (fils > 50) {
+                System.out.println("Massa fils, posa max 50 si no estarem tota la tarda");
+                fils = scan.nextInt();
+            }
+            else {
+                invalid = false;
+            }
+        }
 
-  public static void main (String [] pArguments) {
-      Scanner scan =new Scanner(System.in);
-      System.out.println("Quants fils vols crear");
-      int fils= scan.nextInt();
-      int primer=100;
-      int afegir=100;
-      System.out.println("Iniciant fil principal");
+        int temps = 100;
+        System.out.println("Iniciant fil principal");
 
-      for(int i=0;i<fils;i++){
-          cFil vObjecteFil = new cFil ("#"+(i+1));//Sino quede feo
-          vObjecteFil.sTemporitzacio(primer+(i*afegir)); //No se si aixi ho estic fent be
-          Thread vFil=new Thread(vObjecteFil);
-          vFil.start();
-          System.out.println("Fil secundari iniciat "+ vObjecteFil.gNomFil());
-      }
+        for(int i=0;i<fils;i++){
+            cFil vObjecteFil = new cFil ("#"+(i+1));//Sino quede feo
+            vObjecteFil.sTemporitzacio(i*temps); //No se si aixi ho estic fent be
+            Thread vFil=new Thread(vObjecteFil);
+            vFil.start();
+            System.out.println("Fil secundari iniciat "+ vObjecteFil.gNomFil());
+        }
 
-
-
-
-
-      try {
-          for (int vComptador = 0; vComptador < 10; vComptador ++) {
-            Thread.sleep (500);
-            System.out.println ("Despertant aturada " + vComptador + " procès principal");
-          }
-      }
-      catch (InterruptedException pExcepcio) {
-          System.out.println("Interrompent execució procès principal");
-      }
-      System.out.println("Acabant execució procès principal");
-  }
-
+        try {
+            for (int vComptador = 0; vComptador < 10; vComptador ++) {
+                Thread.sleep(temps * fils + 1); // aixi sempre acabara despres (:
+                System.out.println ("Despertant aturada " + vComptador + " procès principal");
+            }
+        }
+        catch (InterruptedException pExcepcio) {
+            System.out.println("Interrompent execució procès principal");
+        }
+        System.out.println("Acabant execució procès principal");
+    }
 }
